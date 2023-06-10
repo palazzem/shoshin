51312bd51efc7fab2cc695a1581d35e8b8e4df37
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2023-06-10 15:34:02 +0000

    feat: use whisper-1 model to transcribe text

--- src/shoshin/cli/utils.py
+++ src/shoshin/cli/utils.py
@@ -1,4 +1,6 @@
+import click
 import ffmpeg
+import openai
 
 
 def extract_audio(video_file: str, output_file: str) -> None:
@@ -24,4 +26,31 @@ def extract_audio(video_file: str, output_file: str) -> None:
 
 
 def speech_to_text(audio_file: str) -> str:
-    return "Audio has not been transcribed yet."
+    """Transcribes speech from an audio file using the OpenAI Whisper model.
+
+    Args:
+        audio_file (str): The path to the input audio file.
+
+    Raises:
+        click.ClickException: If the OpenAI API request fails for any reason.
+
+    Returns:
+        str: The transcribed text.
+    """
+    stream = open(audio_file, "rb")
+    try:
+        return openai.Audio.transcribe("whisper-1", stream)["text"]
+    except openai.error.Timeout as e:
+        raise click.ClickException(f"OpenAI API request timed out: {e}")
+    except openai.error.APIError as e:
+        raise click.ClickException(f"OpenAI API returned an API Error: {e}")
+    except openai.error.APIConnectionError as e:
+        raise click.ClickException(f"OpenAI API request failed to connect: {e}")
+    except openai.error.InvalidRequestError as e:
+        raise click.ClickException(f"OpenAI API request was invalid: {e}")
+    except openai.error.AuthenticationError as e:
+        raise click.ClickException(f"OpenAI API request was not authorized: {e}")
+    except openai.error.PermissionError as e:
+        raise click.ClickException(f"OpenAI API request was not permitted: {e}")
+    except openai.error.RateLimitError as e:
+        raise click.ClickException(f"OpenAI API request exceeded rate limit: {e}")
