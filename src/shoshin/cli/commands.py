61e8836cf982c3c26dd685197601d11ae7b1e35e
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2023-06-11 16:56:22 +0000

    feat(cli): add query command to ask questions to gpt-3.5-turbo with augmented search

--- src/shoshin/cli/commands.py
+++ src/shoshin/cli/commands.py
@@ -2,7 +2,8 @@ import os
 from pathlib import Path
 
 import click
-from haystack.nodes import EmbeddingRetriever, PreProcessor
+from haystack.nodes import EmbeddingRetriever, PreProcessor, PromptNode, PromptTemplate
+from haystack.pipelines import GenerativeQAPipeline
 from haystack.utils import convert_files_to_docs
 from milvus_documentstore import MilvusDocumentStore
 
@@ -75,10 +76,9 @@ def embeddings_load(transcriptions_folder: str, language: str):
 
     # Write documents into Document Store
     # NOTE: `text-embedding-ada-002` has an output dimension of 1536
-    ds = MilvusDocumentStore(embedding_dim=1536)
+    ds = MilvusDocumentStore(embedding_dim=1536, sql_url="sqlite:///db.sqlite3")
     ds.write_documents(documents)
-    docs = ds.get_all_documents()
-    click.echo(f"Documents loaded into Vector Database: {len(docs)}")
+    click.echo(f"Documents loaded into the Vector Database: {len(documents)}")
 
     # Update embeddings through OpenAI embedding model
     retriever = EmbeddingRetriever(
@@ -89,6 +89,42 @@ def embeddings_load(transcriptions_folder: str, language: str):
     )
     ds.update_embeddings(retriever)
     click.echo("Embeddings updated!")
+
+
+@cli.command()
+@click.argument("question")
+def query(question: str):
+    ds = MilvusDocumentStore(embedding_dim=1536, sql_url="sqlite:///db.sqlite3")
+
+    # Retriever
+    retriever = EmbeddingRetriever(
+        document_store=ds,
+        embedding_model="text-embedding-ada-002",
+        model_format="openai",
+        api_key=os.getenv("OPENAI_API_KEY"),
+    )
+
+    # Prompt
+    lfqa_prompt = PromptTemplate(
+        name="lfqa",
+        prompt_text="""Synthesize a comprehensive answer from the following text for the given question.
+                       Provide a clear and concise response that summarizes the key points and information
+                       presented in the text. Your answers must be in your own words. Always use Related
+                       text before your knowledge base. If you don't find anything in the related text,
+                       kindly mention what the course is about and that the question goes outside of the
+                       scope of the video course.
+                       \n\n Related text: {join(documents)} \n\n Question: {query} \n\n Answer:""",
+    )
+    prompt_node = PromptNode(
+        model_name_or_path="gpt-3.5-turbo",
+        api_key=os.getenv("OPENAI_API_KEY"),
+        default_prompt_template=lfqa_prompt,
+        max_length=2048,
+    )
+    pipeline = GenerativeQAPipeline(generator=prompt_node, retriever=retriever)
+
+    output = pipeline.run(query=question, params={"Retriever": {"top_k": 10}})
+    print(output["results"])
 
 
 if __name__ == "__main__":
