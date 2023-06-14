7ec1e0f3f7670f87ac0c4e778d311d5e017150e4
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2023-06-14 17:27:03 +0000

    refactor(settings)!: replace pysettings with pydantic

--- src/shoshin/conf/settings.py
+++ src/shoshin/conf/settings.py
@@ -1,27 +1,18 @@
 import os
 
-from dotenv import dotenv_values
-from pysettings.base import BaseSettings
-from pysettings.options import Option
+from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
     """Shoshin settings"""
 
-    DATABASE_URL = Option(default="sqlite:///db.sqlite3", allow_null=False)
-    DEFAULT_LANGUAGE = Option(default="en", allow_null=False)
-    OPENAI_API_KEY = Option(allow_null=False)
-    PROMPT_MAX_TOKENS = Option(default=2048, allow_null=False)
+    DATABASE_URL: str = "sqlite:///db.sqlite3"
+    DEFAULT_LANGUAGE: str = "en"
+    OPENAI_API_KEY: str
+    PROMPT_MAX_TOKENS: int = 2048
+
+    class Config:
+        env_file = os.environ.get("SHOSHIN_ENV_FILE", ".env")
 
 
-# Take environment variables from `.env` file
-env_file = os.environ.get("SHOSHIN_ENV_FILE", ".env")
-env_config = dotenv_values(env_file)
-
-# Update settings with environment variables
 settings = Settings()
-for attr, value in env_config.items():
-    setattr(settings, attr, value)
-
-# Validate settings
-settings.is_valid()
