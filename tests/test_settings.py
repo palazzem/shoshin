fb74d103992c9b2dc8715753268310cd01890dee
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2023-06-14 17:33:23 +0000

    tests(settings): enforce constants in tests

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,5 +1,7 @@
 import os
 
+from shoshin.conf import constants as c
+
 
 def test_dotenv_settings(settings):
     # Test settings are overridden by env.testing file
@@ -8,3 +10,12 @@ def test_dotenv_settings(settings):
     assert settings.DEFAULT_LANGUAGE == "en"
     assert settings.OPENAI_API_KEY == "sk-000000000000000000000000000000000000000000000000"
     assert settings.PROMPT_MAX_TOKENS == 2048
+
+
+def test_constants():
+    # Changing these values will break the application
+    assert c.EMBEDDING_DIM == 1536
+    assert c.EMBEDDING_MODEL == "text-embedding-ada-002"
+    assert c.LLM_MODEL == "gpt-3.5-turbo"
+    assert c.PREPROCESSOR_SPLIT_LENGTH == 100
+    assert c.RETRIEVER_TOP_K == 10
