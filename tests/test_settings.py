c2d65c1aee80779b42bb4c6f5be1f1b714da0618
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2023-06-15 12:37:45 +0000

    feat(db)!: remove DATABASE_URL default; use postgresql instead of sqlite in .env files

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -6,7 +6,7 @@ from shoshin.conf import constants as c
 def test_dotenv_settings(settings):
     # Test settings are overridden by env.testing file
     assert os.environ.get("SHOSHIN_ENV_FILE") == ".env.testing"
-    assert settings.DATABASE_URL == "sqlite:///db.testing.sqlite3"
+    assert settings.DATABASE_URL == "postgresql://dev:dev@localhost:5432/milvus"
     assert settings.DEFAULT_LANGUAGE == "en"
     assert settings.OPENAI_API_KEY == "sk-000000000000000000000000000000000000000000000000"
     assert settings.PROMPT_MAX_TOKENS == 2048
