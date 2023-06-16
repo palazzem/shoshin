87c5fdc9d20ec99d22ad133100e82a0e02949ffc
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2023-06-16 14:36:16 +0000

    tests(fixtures): add server, audio and output file fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,14 @@
 import pytest
+import responses
 
 from shoshin.conf import settings as global_settings
+
+
+@pytest.fixture
+def server():
+    """Create a `responses` mock."""
+    with responses.RequestsMock() as resp:
+        yield resp
 
 
 @pytest.fixture(scope="function")
@@ -27,3 +35,15 @@ def ffmpeg(mocker):
         The mocked `ffmpeg` module.
     """
     return mocker.patch("shoshin.pipeline.processors.ffmpeg")
+
+
+@pytest.fixture(scope="session")
+def audio_file(tmp_path_factory):
+    path = tmp_path_factory.mktemp("audio") / "audio.mp3"
+    open(path, "w").close()
+    yield path
+
+
+@pytest.fixture(scope="session")
+def output_file(tmp_path_factory):
+    yield tmp_path_factory.mktemp("output") / "output.bin"
